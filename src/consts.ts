2c7b9c05e472df618fa4fc9b2c1c2f7a0209ef45
Mr.yao <lily02@rd.netease.com>
2023-05-04 13:42:40 +0800

    feat: 整理基本结构

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '⛈️';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '禄尧的博客';
+export const SITE_DESCRIPTION = '欢迎来到禄尧的博客!';
---
+++ src/content/blog/what-is-astro.md
@@ -0,0 +1,8 @@
+---
+title: "astro"
+description: "Lorem ipsum dolor sit amet"
+pubDate: "Jul 08 2022"
+heroImage: ""
+---
+
+[Astro](https://astro.build/): `Build the web you want`
